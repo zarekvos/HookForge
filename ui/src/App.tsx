31f1f25e233df665c867238a1e94b3ee8e014229
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-01-03 20:35:54 +0100

    :sparkles: feat: add header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -13,10 +13,9 @@ import {
 function App() {
   return (
     <>
-      <div className=" flex flex-col items-center p-10">
-        <Card className=" w-full bg-black opacity-80">
-          <div className=" absolute right-0">Add GH button TODO</div>
-          <h1 className= " text-4xl mt-8 font-semibold">Online Uniswap V4 Hook Address Miner</h1>
+      <div className=" flex flex-col items-center px-28  pt-12 mt-12">
+        <Card className=" w-full bg-black bg-opacity-80 border-pink-500">
+          <h1 className= " text-4xl mt-8 font-semibold">Uniswap V4 Hook Address Miner Online Tool</h1>
           
           <div className= " mx-5 h-96 bg-red-700">
             TODO Add form to mine
--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Header } from "@/components/header";
 import "./index.css";
-import AsciiNoise  from "./lib/asciinoise/asciinoise.tsx";
+import AsciiNoise from "./lib/asciinoise/asciinoise.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AsciiNoise />
-    <App />
+    <Header />
+    <App/>
   </StrictMode>
 );
