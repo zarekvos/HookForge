c0a77ddb724a1ddca30645f9ebdd2a9f40cb8a32
GianfrancoBazzani <bazzanigianfranco@gmail.com>
2025-01-13 23:21:14 +0100

    :hammer: fix: remove helmet

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,17 +3,12 @@ import { createRoot } from "react-dom/client";
 import { Header } from "./components/header";
 import "./index.css";
 import AsciiNoise from "./lib/asciinoise/asciinoise.tsx";
-import { HelmetProvider } from "react-helmet-async";
-import MetaTags from "./components/metatags.tsx";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      <MetaTags />
       <AsciiNoise />
       <Header />
       <App />
-    </HelmetProvider>
   </StrictMode>
 );
